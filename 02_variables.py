48a0d62a4c701ed842462bb5197e73c8b150d172
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2021-08-02 13:09:39 +1200

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,10 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 7
+
+# this variable has a problem
+my_name = "Sofia Uy"
